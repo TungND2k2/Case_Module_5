33ab56994f91f19fe52dddc1b8dd59504ceadaf2
lamnguyen1603 <nguyenlam2521603@gmail.com>
2023-02-24 10:45:55 +0700

    xong router thieu userRouter

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -4,8 +4,10 @@ import {postRouter} from "./postRouter";
 import {jobRouter} from "./jobRouter";
 import {jobDetailRouter} from "./jobDetailRouter";
 import postController from "../controllers/postController";
+import employerRouter from "./employer";
 export const router = Router()
 router.get('/find-by-name', postController.searchPost);
+router.use('/employer',employerRouter)
 router.use('/posts',postRouter);
 router.use('/users',userRouter);
 router.use('/jobs',jobRouter)
