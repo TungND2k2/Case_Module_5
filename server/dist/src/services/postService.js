6af3f0c9bd0635245a98a1d989e84ebae340e3d8
ChatNghe <namnt98@gmail.com>
2023-03-01 10:18:22 +0700

    sua BE search

--- server/dist/src/services/postService.js
+++ server/dist/src/services/postService.js
@@ -19,14 +19,30 @@ class PostService {
                           e.employerName,
                           image,
                           j.jobName
-                   from post p join employer e on p.idEmployer = e.idEmployer
-                               join job_detail jd on p.idPost = jd.postId
-                               join job j on jd.jobId = j.jobId
-                   order by idPost DESC limit 6`;
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
+                   order by idPost DESC`;
             let post1 = await this.postRepository.query(sql);
             const result = post1.reduce((acc1, { idPost, title, salary, workLocation, position, experience, workTime, endTime, description, recruitmentsNumber, status, employerName, image, jobName }) => {
                 var _a;
-                (_a = acc1[idPost]) !== null && _a !== void 0 ? _a : (acc1[idPost] = { idPost: idPost, title: title, salary: salary, workLocation: workLocation, position: position, experience: experience, workTime: workTime, endTime: endTime, description: description, recruitmentsNumber: recruitmentsNumber, status: status, employerName: employerName, image: image, jobName: [] });
+                (_a = acc1[idPost]) !== null && _a !== void 0 ? _a : (acc1[idPost] = {
+                    idPost: idPost,
+                    title: title,
+                    salary: salary,
+                    workLocation: workLocation,
+                    position: position,
+                    experience: experience,
+                    workTime: workTime,
+                    endTime: endTime,
+                    description: description,
+                    recruitmentsNumber: recruitmentsNumber,
+                    status: status,
+                    employerName: employerName,
+                    image: image,
+                    jobName: []
+                });
                 if (Array.isArray(jobName))
                     acc1[idPost].jobName = acc1[idPost].jobName.concat(jobName);
                 else
@@ -37,7 +53,8 @@ class PostService {
         };
         this.countPosts = async () => {
             try {
-                let sql = `select count(idPost) from post`;
+                let sql = `select count(idPost)
+                       from post`;
                 let posts = await this.postRepository.query(sql);
                 return posts;
             }
@@ -66,6 +83,7 @@ class PostService {
             }
         };
         this.search = async (req, res, limit, offset) => {
+            console.log(req.query);
             let sql = `select idPost,
                           title,
                           salary,
@@ -80,12 +98,16 @@ class PostService {
                           e.employerName,
                           image,
                           j.jobName
-                   from post p join employer e on p.idEmployer = e.idEmployer
-                               join job_detail jd on p.idPost = jd.postId
-                               join job j on jd.jobId = j.jobId 
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
                    where (1 = 1) `;
             if (req.query.title !== undefined) {
                 sql += `and title like '%${req.query.title}'`;
+            }
+            if (req.query.idPost !== undefined) {
+                sql += `and idPost like '%${req.query.idPost}'`;
             }
             if (req.query.salary !== undefined) {
                 sql += `and salary like '%${req.query.salary}'`;
@@ -102,21 +124,105 @@ class PostService {
             if (req.query.experience !== undefined) {
                 sql += `and experience like '%${req.query.experience}'`;
             }
-            if (req.query.jobName !== undefined) {
-                sql += `and jobName like '%${req.query.jobName}'`;
+            let listJob = [];
+            if (Array.isArray(req.query.jobName)) {
+                listJob = req.query.jobName;
             }
-            sql += `order by idPost DESC limit ${limit} offset ${offset}`;
+            else {
+                listJob.push(req.query.jobName);
+            }
+            if (req.query.jobName !== undefined) {
+                sql += `and jobName like (1=1)`;
+                for (let i = 0; i < listJob.length; i++) {
+                    sql += `or jobName like '%${listJob[i]}'`;
+                }
+            }
+            sql += `order by idPost DESC`;
             let post = await this.postRepository.query(sql);
             const result = post.reduce((acc, { idPost, title, salary, workLocation, position, experience, workTime, endTime, description, recruitmentsNumber, status, employerName, image, jobName }) => {
                 var _a;
-                (_a = acc[idPost]) !== null && _a !== void 0 ? _a : (acc[idPost] = { idPost: idPost, title: title, salary: salary, workLocation: workLocation, position: position, experience: experience, workTime: workTime, endTime: endTime, description: description, recruitmentsNumber: recruitmentsNumber, status: status, employerName: employerName, image: image, jobName: [] });
+                (_a = acc[idPost]) !== null && _a !== void 0 ? _a : (acc[idPost] = {
+                    idPost: idPost,
+                    title: title,
+                    salary: salary,
+                    workLocation: workLocation,
+                    position: position,
+                    experience: experience,
+                    workTime: workTime,
+                    endTime: endTime,
+                    description: description,
+                    recruitmentsNumber: recruitmentsNumber,
+                    status: status,
+                    employerName: employerName,
+                    image: image,
+                    jobName: []
+                });
                 if (Array.isArray(jobName))
                     acc[idPost].jobName = acc[idPost].jobName.concat(jobName);
                 else
                     acc[idPost].jobName.push(jobName);
                 return acc;
             }, {});
-            return Object.values(result);
+            let listPost = [];
+            for (let i = 0; i < Object.values(result).length; i++) {
+                if (Object.values(result)[i].jobName.length === listJob.length) {
+                    listPost.push(Object.values(result)[i]);
+                }
+            }
+            let sql1 = `select idPost,
+                          title,
+                          salary,
+                          workLocation,
+                          position,
+                          experience,
+                          workTime,
+                          endTime,
+                          p.description,
+                          recruitmentsNumber,
+                          p.status,
+                          e.employerName,
+                          image,
+                          j.jobName
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
+                   order by idPost DESC`;
+            let post1 = await this.postRepository.query(sql1);
+            const result1 = post1.reduce((acc1, { idPost, title, salary, workLocation, position, experience, workTime, endTime, description, recruitmentsNumber, status, employerName, image, jobName }) => {
+                var _a;
+                (_a = acc1[idPost]) !== null && _a !== void 0 ? _a : (acc1[idPost] = {
+                    idPost: idPost,
+                    title: title,
+                    salary: salary,
+                    workLocation: workLocation,
+                    position: position,
+                    experience: experience,
+                    workTime: workTime,
+                    endTime: endTime,
+                    description: description,
+                    recruitmentsNumber: recruitmentsNumber,
+                    status: status,
+                    employerName: employerName,
+                    image: image,
+                    jobName: []
+                });
+                if (Array.isArray(jobName))
+                    acc1[idPost].jobName = acc1[idPost].jobName.concat(jobName);
+                else
+                    acc1[idPost].jobName.push(jobName);
+                return acc1;
+            }, {});
+            let allPost = Object.values(result1);
+            let searchPost = [];
+            for (let i = 0; i < allPost.length; i++) {
+                for (let j = 0; j < listPost.length; j++) {
+                    if (listPost[j].idPost === allPost[i].idPost) {
+                        searchPost.push(allPost[i]);
+                    }
+                }
+            }
+            return searchPost;
         };
         this.postRepository = data_source_1.AppDataSource.getRepository(post_1.Post);
     }
--- server/src/services/postService.ts
+++ server/src/services/postService.ts
@@ -26,29 +26,59 @@ class PostService {
                           e.employerName,
                           image,
                           j.jobName
-                   from post p join employer e on p.idEmployer = e.idEmployer
-                               join job_detail jd on p.idPost = jd.postId
-                               join job j on jd.jobId = j.jobId
-                   order by idPost DESC limit 6`
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
+                   order by idPost DESC`
         let post1 = await this.postRepository.query(sql);
-        const result = post1.reduce((acc1, {idPost,title,salary,workLocation,position,experience,workTime,endTime,description,recruitmentsNumber,status,employerName,image, jobName}) => {
-            acc1[idPost] ??= {idPost: idPost,title:title,salary:salary,workLocation:workLocation,position:position,experience:experience,workTime:workTime,endTime:endTime,description:description,recruitmentsNumber:recruitmentsNumber,status:status,employerName:employerName,image:image, jobName: []};
-            if(Array.isArray(jobName)) // if it's array type then concat
+        const result = post1.reduce((acc1, {
+            idPost,
+            title,
+            salary,
+            workLocation,
+            position,
+            experience,
+            workTime,
+            endTime,
+            description,
+            recruitmentsNumber,
+            status,
+            employerName,
+            image,
+            jobName
+        }) => {
+            acc1[idPost] ??= {
+                idPost: idPost,
+                title: title,
+                salary: salary,
+                workLocation: workLocation,
+                position: position,
+                experience: experience,
+                workTime: workTime,
+                endTime: endTime,
+                description: description,
+                recruitmentsNumber: recruitmentsNumber,
+                status: status,
+                employerName: employerName,
+                image: image,
+                jobName: []
+            };
+            if (Array.isArray(jobName)) // if it's array type then concat
                 acc1[idPost].jobName = acc1[idPost].jobName.concat(jobName);
             else
                 acc1[idPost].jobName.push(jobName);
-
-
             return acc1;
         }, {});
         return Object.values(result);
     }
-    countPosts=async ( ) => {
+    countPosts = async () => {
         try {
-            let sql=`select count(idPost) from post`
-            let posts=await this.postRepository.query(sql)
+            let sql = `select count(idPost)
+                       from post`
+            let posts = await this.postRepository.query(sql)
             return posts
-        }catch (e) {
+        } catch (e) {
             console.log(e)
         }
     }
@@ -73,8 +103,8 @@ class PostService {
             return this.postRepository.delete({idPost: id});
         }
     }
-    search = async (req: Request, res: Response,limit, offset) => {
-
+    search = async (req: Request, res: Response, limit, offset) => {
+        console.log(req.query)
         let sql = `select idPost,
                           title,
                           salary,
@@ -89,12 +119,16 @@ class PostService {
                           e.employerName,
                           image,
                           j.jobName
-                   from post p join employer e on p.idEmployer = e.idEmployer
-                               join job_detail jd on p.idPost = jd.postId
-                               join job j on jd.jobId = j.jobId 
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
                    where (1 = 1) `
         if (req.query.title !== undefined) {
             sql += `and title like '%${req.query.title}'`
+        }
+        if (req.query.idPost !== undefined) {
+            sql += `and idPost like '%${req.query.idPost}'`
         }
         if (req.query.salary !== undefined) {
             sql += `and salary like '%${req.query.salary}'`
@@ -111,26 +145,138 @@ class PostService {
         if (req.query.experience !== undefined) {
             sql += `and experience like '%${req.query.experience}'`
         }
-        if (req.query.jobName !== undefined) {
-            sql += `and jobName like '%${req.query.jobName}'`
-
+        let listJob = []
+        if (Array.isArray(req.query.jobName)){
+            listJob = req.query.jobName
+        }else {
+            listJob.push(req.query.jobName)
+        }
+        if (req.query.jobName !== undefined ) {
+            sql += `and jobName like (1=1)`
+            for (let i = 0; i < listJob.length; i++) {
+                sql+= `or jobName like '%${listJob[i]}'`
+            }
         }
 
-        sql += `order by idPost DESC limit ${limit} offset ${offset}`
+        sql += `order by idPost DESC`
         let post = await this.postRepository.query(sql);
-        const result = post.reduce((acc, {idPost,title,salary,workLocation,position,experience,workTime,endTime,description,recruitmentsNumber,status,employerName,image, jobName}) => {
-            acc[idPost] ??= {idPost: idPost,title:title,salary:salary,workLocation:workLocation,position:position,experience:experience,workTime:workTime,endTime:endTime,description:description,recruitmentsNumber:recruitmentsNumber,status:status,employerName:employerName,image:image, jobName: []};
-            if(Array.isArray(jobName)) // if it's array type then concat
+        const result = post.reduce((acc, {
+            idPost,
+            title,
+            salary,
+            workLocation,
+            position,
+            experience,
+            workTime,
+            endTime,
+            description,
+            recruitmentsNumber,
+            status,
+            employerName,
+            image,
+            jobName
+        }) => {
+            acc[idPost] ??= {
+                idPost: idPost,
+                title: title,
+                salary: salary,
+                workLocation: workLocation,
+                position: position,
+                experience: experience,
+                workTime: workTime,
+                endTime: endTime,
+                description: description,
+                recruitmentsNumber: recruitmentsNumber,
+                status: status,
+                employerName: employerName,
+                image: image,
+                jobName: []
+            };
+            if (Array.isArray(jobName)) // if it's array type then concat
                 acc[idPost].jobName = acc[idPost].jobName.concat(jobName);
             else
                 acc[idPost].jobName.push(jobName);
-
-
             return acc;
         }, {});
-        return Object.values(result);
+        let listPost = []
+        for (let i = 0; i < Object.values(result).length; i++) {
+            // @ts-ignore
+            if (Object.values(result)[i].jobName.length === listJob.length){
+                listPost.push(Object.values(result)[i])
+            }
+        }
+        let sql1 = `select idPost,
+                          title,
+                          salary,
+                          workLocation,
+                          position,
+                          experience,
+                          workTime,
+                          endTime,
+                          p.description,
+                          recruitmentsNumber,
+                          p.status,
+                          e.employerName,
+                          image,
+                          j.jobName
+                   from post p
+                            join employer e on p.idEmployer = e.idEmployer
+                            join job_detail jd on p.idPost = jd.postId
+                            join job j on jd.jobId = j.jobId
+                   order by idPost DESC`
+        let post1 = await this.postRepository.query(sql1);
+        const result1 = post1.reduce((acc1, {
+            idPost,
+            title,
+            salary,
+            workLocation,
+            position,
+            experience,
+            workTime,
+            endTime,
+            description,
+            recruitmentsNumber,
+            status,
+            employerName,
+            image,
+            jobName
+        }) => {
+            acc1[idPost] ??= {
+                idPost: idPost,
+                title: title,
+                salary: salary,
+                workLocation: workLocation,
+                position: position,
+                experience: experience,
+                workTime: workTime,
+                endTime: endTime,
+                description: description,
+                recruitmentsNumber: recruitmentsNumber,
+                status: status,
+                employerName: employerName,
+                image: image,
+                jobName: []
+            };
+            if (Array.isArray(jobName)) // if it's array type then concat
+                acc1[idPost].jobName = acc1[idPost].jobName.concat(jobName);
+            else
+                acc1[idPost].jobName.push(jobName);
+            return acc1;
+        }, {});
+
+        let allPost = Object.values(result1)
+        let searchPost = []
+        for (let i = 0; i < allPost.length; i++) {
+            for (let j = 0; j < listPost.length; j++) {
+                // @ts-ignore
+                if(listPost[j].idPost === allPost[i].idPost){
+                    searchPost.push(allPost[i])
+                }
+            }
+        }
+
+        return searchPost;
     }
 }
-
 
 export default new PostService();
